1194f0c137b7fc4456e0eb425677abd7069e5b0d
Gowri Nambiar <gowri.nambi@gmail.com>
2013-11-30 17:14:55 -0500

    users form and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RainforestAjax::Application.routes.draw do
+  resources :users, :only => [:new, :create, :show]
   resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131130170826) do
+ActiveRecord::Schema.define(version: 20131130213921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20131130170826) do
     t.string   "name"
     t.text     "description"
     t.integer  "price_in_cents"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
